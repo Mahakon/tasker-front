1abd9cfa32a48c7fb8a38793bf82a161aced6417
Mariia Kononenko <mkon.masha@gmail.com>
2018-04-22 12:57:57 +0300

    fjkf

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,10 @@
-/*export const host = 'https://c9751a4b.ngrok.io/api/';
-export const wsshost = 'wss://c9751a4b.ngrok.io';
-export const static_host = 'https://8e9832b5.ngrok.io/';*/
+export const host = 'https://dd171f26.ngrok.io/api/';
+export const wsshost = 'wss://dd171f26.ngrok.io';
+export const static_host = 'https://dd171f26.ngrok.io/';
 export const mode = 'production';
 export const login = 'maha';
 export const password = 'maha';
 
-export const host = 'https://bot-tasker.info/api/';
+/*export const host = 'https://bot-tasker.info/api/';
 export const wsshost = 'wss://bot-tasker.info';
-export const static_host = 'https://bot-tasker.info/';
+export const static_host = 'https://bot-tasker.info/';*/
