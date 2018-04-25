992f0d0a2734691f2a0f7ac8198dd3eeaae7d9cf
Stanislav Gorovoy <cnfc19932@gmail.com>
2018-04-25 13:44:02 +0300

    чат

--- src/app/cabinet/dashboard/comment/comment.component.ts
+++ src/app/cabinet/dashboard/comment/comment.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TaskComment} from '../../../services/cabinet/dashboard/task.service';
+import {static_host} from '../../../config';
 
 @Component({
   selector: 'app-comment',
@@ -10,10 +11,13 @@ export class CommentComponent implements OnInit {
 
   @Input() comment: TaskComment;
   @Input() curUserId: number;
-
+  static_host;
   constructor() { }
 
   ngOnInit() {
+    this.static_host  = static_host;
+    // console.log('Комментарий', this.comment);
+   //  console.log(this.comment.user_id, this.curUserId);
   }
 
 }
